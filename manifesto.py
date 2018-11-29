d3985b1472326691aef44293a37aee696162570e
Carlo Sugatan <carlosugatan@gmail.com>
2018-11-29 15:46:53 -0500

    Got all data, just need to clean it up

--- manifesto.py
+++ manifesto.py
@@ -10,41 +10,60 @@ import textract
 from datetime import datetime
 
 # Textract
-# dylan_roof = textract.process("/Users/Carlo/Google Drive/FA18 Classes/SI508/Assignments - HW/Final Project/Manifesto-Project/Dylan_Roof.pdf", encoding='utf_8')
-#
+# Dylan Roof
+dylan_roof = textract.process("/Users/Carlo/Google Drive/FA18 Classes/SI508/Assignments - HW/Final Project/Manifesto-Project/Dylan_Roof.pdf", encoding='utf_8')
+
+manifesto_dict = {}
+
+manifesto_dict['Dylan Roof'] = {}
+manifesto_dict['Dylan Roof']['Dylan Roof Manifesto'] = dylan_roof
+
+print(manifesto_dict)
+# Elliot Rodger
 # elliot_rodger = textract.process("/Users/Carlo/Google Drive/FA18 Classes/SI508/Assignments - HW/Final Project/Manifesto-Project/Elliot_Rodger.pdf", encoding='utf_8')
-#
+
+#Seung Hui Cho
 # seung_hui_cho = textract.process("/Users/Carlo/Google Drive/FA18 Classes/SI508/Assignments - HW/Final Project/Manifesto-Project/seung_hui_cho.pdf", encoding='utf_8')
-#
+
+# Anders Breivik
 # anders_breivik = textract.process("/Users/Carlo/Google Drive/FA18 Classes/SI508/Assignments - HW/Final Project/Manifesto-Project/Anders_Behring_Breivik.pdf", encoding='utf_8')
-#
+
+# Eric Auvinen
 # eric_auvinen = textract.process("/Users/Carlo/Google Drive/FA18 Classes/SI508/Assignments - HW/Final Project/Manifesto-Project/Auvinen.doc", encoding='utf_8')
 
+# Ted Kaczynski
+# ted_kaczynski = textract.process("/Users/Carlo/Google Drive/FA18 Classes/SI508/Assignments - HW/Final Project/Manifesto-Project/Ted_Kaczynski.pdf", encoding='utf_8')
+# print(ted_kaczynski)
+
 # Web Scraping
-# CACHE_FNAME = "manifesto-data.json"
-# Unabomber
-# url_to_scrape = "https://www.washingtonpost.com/wp-srv/national/longterm/unabomber/manifesto.text.htm"
+CACHE_FNAME = "manifesto-data.json"
 
 # Christopher Dorner
 # url_to_scrape = "http://www.laist.com/2013/02/07/christopher_dorners_manifesto_in_fu.php"
+# manifesto_text = soup.find('blockquote').text
+# print(manifesto_text)
 
 # Adkission Manifesto
 # url_to_scrape = "https://faith17983.wordpress.com/2015/05/31/the-adkisson-manifesto/"
+# manifesto_text = soup.find('div', class_ = 'entry-content')
+# manifesto_text = manifesto_text.text
 
 # primary_cache = Cache(CACHE_FNAME)
-
+#
 # while primary_cache.get(url_to_scrape) is None:
 # 	data = requests.get(url_to_scrape)
 # 	html_text = data.text
 # 	primary_cache.set(url_to_scrape,html_text)
 #
 # soup = BeautifulSoup(primary_cache.get(url_to_scrape), features="html.parser")
-# page_title = soup.title.text
-# print(page_title)
-# dict = {}
-# dict[url_to_scrape] = page_title
-# print(dict)
-# # print(soup.find("a")) # For example
+# # page_title = soup.title.text
+# # print(page_title)
+# manifesto_text = soup.find('div', class_ = 'entry-content')
+# manifesto_text = manifesto_text.text
+# # manifesto_text = soup.find_all('td')
+# print(manifesto_text)
+
+# print(soup.find("a")) # For example
 
 
 
